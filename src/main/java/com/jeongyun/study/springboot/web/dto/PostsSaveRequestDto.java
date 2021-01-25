1b34548cca26e6ce19c3336ab605570f16a3dae9
jeongyuneo <piq2255@gmail.com>
2021-01-25 18:12:16 +0900

    feat: 등록/수정/조회 API 만들기

---
+++ src/main/java/com/jeongyun/study/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,35 @@
+package com.jeongyun.study.springboot.web.dto;
+
+import com.jeongyun.study.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsSaveRequestDto(Posts entity) {
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
