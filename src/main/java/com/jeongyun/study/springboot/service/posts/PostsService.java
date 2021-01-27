03102241b63e600d1f7a768b86f82c632b5cdb75
jeongyuneo <piq2255@gmail.com>
2021-01-27 15:33:58 +0900

    feat: 전체 조회 화면 만들기

--- src/main/java/com/jeongyun/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/jeongyun/study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jeongyun.study.springboot.service.posts;
 
 import com.jeongyun.study.springboot.domain.posts.Posts;
 import com.jeongyun.study.springboot.domain.posts.PostsRepository;
+import com.jeongyun.study.springboot.web.dto.PostsListResponseDto;
 import com.jeongyun.study.springboot.web.dto.PostsResponseDto;
 import com.jeongyun.study.springboot.web.dto.PostsSaveRequestDto;
 import com.jeongyun.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,17 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
